c317f725d33e7847b538c9e46500014b213febe6
Martin Billinger <mbillingr@users.noreply.github.com>
2020-04-03 19:57:15 +0200

    chapter 6

---
+++ rust/examples/chapter-06.rs
@@ -0,0 +1,48 @@
+use raytracing::canvas::Canvas;
+use raytracing::color::color;
+use raytracing::lights::PointLight;
+use raytracing::materials::Phong;
+use raytracing::ray::{hit, Intersection, Ray};
+use raytracing::shapes::{Shape, Sphere};
+use raytracing::tuple::{point, vector};
+use std::fs::File;
+
+fn main() {
+    let (width, height) = (512, 512);
+    let mut canvas = Canvas::new(width, height);
+    canvas.life_view("Canvas view");
+
+    let mut sphere = Sphere::new();
+    sphere.set_material(Phong::new(color(0.2, 0.8, 0.9), 0.1, 0.9, 0.9, 20.0));
+
+    let scene = vec![sphere];
+    let light = PointLight::new(point(1, 9, -10), color(1, 1, 1));
+
+    let eye = point(0, 0, -10);
+    let look = vector(0, 0, 3); // magnitude = focal length
+
+    for j in 0..height {
+        let y = 0.5 - j as f64 / (height - 1) as f64;
+        for i in 0..width {
+            let x = i as f64 / (width - 1) as f64 - 0.5;
+            let ray = Ray::new(eye, (look + vector(x, y, 0)).normalized());
+
+            let intersections: Vec<_> = scene
+                .iter()
+                .flat_map(|shape| shape.intersect(&ray))
+                .collect();
+            let h = hit(&intersections);
+
+            if let Some(Intersection { t, obj }) = h {
+                let p = ray.position(t);
+                let eyev = -ray.direction();
+                let normalv = obj.normal_at(p);
+                let color = obj.material().lighting(&light, p, eyev, normalv);
+                canvas.set_pixel(i, j, color);
+            }
+        }
+    }
+
+    let mut f = File::create("pictures/chapter-06.ppm").unwrap();
+    canvas.write_ppm(&mut f).unwrap();
+}
---
+++ scheme/chapter06.scm
@@ -0,0 +1,54 @@
+(import (scheme base)
+        (scheme write)
+        (scheme file)
+        (raytrace tuple)
+        (raytrace canvas)
+        (raytrace shapes)
+        (raytrace ray)
+        (raytrace matrix)
+        (raytrace lights)
+        (raytrace material))
+
+(define (cast ray scene light)
+  (if (null? scene)
+      (color 0 0 0)
+      (let ((xs (intersect (car scene) ray)))
+        (if (null? xs)
+            (cast ray (cdr scene) light)
+            (begin
+              (let* ((h (hit xs))
+                     (t (intersection-t h))
+                     (mat ((intersection-object h) 'material))
+                     (point (ray-position ray t))
+                     (eyev (tuple-neg (ray-direction ray)))
+                     (normalv ((intersection-object h) 'normal-at point)))
+                (lighting mat light point eyev normalv)))))))
+
+(define light (point-light (point 1 9 -10) (color 1 1 1)))
+(define scene (list (sphere)))
+(define eye (point 0 0 -10))
+(define focal-point -7)
+
+(define (render c)
+  (let ((w (canvas-width c))
+        (h (canvas-height c)))
+    (let loop ((x 0) (y 0))
+      (cond ((= y h) c)
+            ((= x w)
+             (loop 0 (+ y 1)))
+            (else
+              (let* ((r (ray eye
+                             (normalize (tuple-sub
+                                          (point (- (/ x w) 0.5)
+                                                 (- 0.5 (/ y h))
+                                                 focal-point)
+                                          eye))))
+                     (color (cast r scene light)))
+                (pixel-set! c x y color)
+                (loop (+ x 1) y)))))))
+
+(define image (render (canvas 200 200)))
+
+(call-with-output-file "chapter06.ppm"
+  (lambda (f)
+    (display (canvas->ppm image) f)))
