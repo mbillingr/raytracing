d279704173cab9a07aafb89521e23d455b5c5d64
Martin Billinger <flkazemakase@gmail.com>
2020-05-13 18:06:26 +0200

    merge photon map examples

--- rust/examples/photon-map.rs
+++ rust/examples/photon-map.rs
@@ -2,8 +2,8 @@ use raytracing::camera::Camera;
 use raytracing::color::color;
 use raytracing::lights::RealisticPointLight;
 use raytracing::materials::Phong;
-use raytracing::matrix::{rotation_x, rotation_z, scaling, translation};
-use raytracing::shapes::{plane, sphere};
+use raytracing::matrix::{rotation_x, rotation_y, rotation_z, scaling, translation};
+use raytracing::shapes::{cube, plane, sphere};
 use raytracing::tuple::{point, vector};
 use raytracing::world::World;
 use std::f64::consts::PI;
@@ -40,7 +40,11 @@ fn main() {
         .with_material(white_material.clone())
         .with_transform(translation(0, 0, -5) * rotation_x(PI / 2.0));
 
-    let box1 = sphere()
+    let box1 = cube()
+        .with_material(white_material.clone())
+        .with_transform(translation(-2, 2, 2) * rotation_y(0.8) * scaling(1, 2, 1));
+
+    let ball = sphere()
         .with_material(
             Phong::default()
                 .with_rgb(1.0, 1.0, 1.0)
@@ -60,6 +64,7 @@ fn main() {
     world.add_item(right_wall);
     world.add_item(front_wall);
     world.add_item(box1);
+    world.add_item(ball);
 
     world.finalize_scene();
 
@@ -67,16 +72,16 @@ fn main() {
     let to = point(0, 5, 0);
 
     let mut camera = Camera::new(600, 600, PI / 2.0).with_view_transform(from, to, vector(0, 1, 0));
-    camera.set_allowed_standard_error(1e2);
-    camera.set_min_samples(1);
+    camera.set_allowed_standard_error(1e-2);
+    camera.set_min_samples(10);
 
     world.enable_direct_illumination(true);
     world.enable_direct_photon_map(false);
     world.enable_diffuse_photon_map(false);
     world.enable_caustic_photon_map(false);
-    let image = camera.render_live(&world, "Photon Map 02");
+    let image = camera.render_live(&world, "Photon Map Example: direct light only");
 
-    let mut f = File::create("pictures/photon-map-02a.png").unwrap();
+    let mut f = File::create("pictures/photon-map-01-trace_direct_only.png").unwrap();
     image.write_png(&mut f).unwrap();
     log::debug!(
         "Average brightness (ray trace): {:?}",
@@ -87,9 +92,9 @@ fn main() {
     world.enable_direct_photon_map(true);
     world.enable_diffuse_photon_map(false);
     world.enable_caustic_photon_map(true);
-    world.compute_photon_map(75_000_000, 1_000);
-    let image = camera.render_live(&world, "Photon Map 02");
-    let mut f = File::create("pictures/photon-map-02b.png").unwrap();
+    world.compute_photon_map(75_000_000, 100);
+    let image = camera.render_live(&world, "Photon Map Example: direct and caustic photons");
+    let mut f = File::create("pictures/photon-map-02-direct_and_caustic_photons.png").unwrap();
     image.write_png(&mut f).unwrap();
     log::debug!(
         "Average brightness (photon map): {:?}",
@@ -100,9 +105,14 @@ fn main() {
     world.enable_direct_photon_map(false);
     world.enable_diffuse_photon_map(true);
     world.enable_caustic_photon_map(true);
-    world.compute_photon_map(75_000_000, 1_000);
-    let image = camera.render_live(&world, "Photon Map 02");
-    let mut f = File::create("pictures/photon-map-02c.png").unwrap();
+    world.compute_photon_map(75_000_000, 100);
+    let image = camera.render_live(
+        &world,
+        "Photon Map Example: direct light and global illumination",
+    );
+    let mut f =
+        File::create("pictures/photon-map-03_traced_direct_diffuse_and_caustic_photons.png")
+            .unwrap();
     image.write_png(&mut f).unwrap();
     log::debug!(
         "Average brightness (ray + indirect photons): {:?}",
